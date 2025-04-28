fc751dfa503296730f7b9a6aa397dd1fd2eeefd0
darrylrachel <racheldarryl@icloud.com>
2025-04-28 13:58:53 -0700

    update: Updated favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,13 @@ export default function RootLayout({
   return (
     <html lang="en" className="light scroll-smooth">
       <head>
-        <link rel="icon" href="/favicon.png" />
+        {/* <link rel="icon" href="/favicon.png" /> */}
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="ElevatrLabs" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body className={`${inter.className} bg-white text-charcoal min-h-screen flex flex-col relative`}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
